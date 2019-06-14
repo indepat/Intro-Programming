86819aaf3bd729b3289c8134794ace1ae4459401
Patrick Indekwe <indepat1@gmail.com>
2019-06-14 13:00:10 -0400

    Add Exercices for Arrays

---
+++ 6_Arrays/Arrays_Exercise1.rb
@@ -0,0 +1,9 @@
+#Below we have given you an array and a number.
+#Write a program that checks to see if the number
+#appears in the array.
+arr = [1, 3, 5, 7, 9, 11]
+arr.include?(3)
+
+#Done on irb
+
+#returned "True"
